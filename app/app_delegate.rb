503e830eea0828f443f4b5b06276ec5d3ac91baa
John Moon <johnmoonyy@gmail.com>
2014-01-26 00:31:47 -0500

    Add button to find random latitude and longitude

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,6 +4,41 @@ class AppDelegate
       @longitude = longitude
       @latitude = latitude
     end
+
+    def loadView
+      camera = GMSCameraPosition.cameraWithLatitude(@latitude, longitude: @longitude, zoom: 3)
+      @map_view = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+      @map_view.myLocationEnabled = true
+      self.view = @map_view
+
+      marker = GMSMarker.alloc.init
+      marker.position = CLLocationCoordinate2DMake(@latitude, @longitude)
+      marker.title = "Washington, D.C"
+      marker.snippet = "United States"
+      marker.map = @map_view
+    end
+  end
+
+  def randomLocation
+    prng = Random.new
+    latitude = prng.rand(-90.0..90.0)
+    longitude = prng.rand(-180.0..180.0)
+    @window.rootViewController = GoogleMapsController.new(longitude, latitude)
+    @window.makeKeyAndVisible
+
+    @add_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @add_button.setTitle("Random Location", forState:UIControlStateNormal)
+    @add_button.sizeToFit
+    @add_button.frame = CGRect.new(
+      [10, @window.frame.size.height - 10 - @add_button.frame.size.height],
+      @add_button.frame.size)
+    @add_button.addTarget(self,
+                          action: 'randomLocation',
+                          forControlEvents: UIControlEventTouchUpInside)
+    @window.addSubview(@add_button)
+  end
+
+
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     latitude = 38.89037
     longitude = -77.031959
@@ -11,6 +46,18 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = GoogleMapsController.new(longitude, latitude)
     @window.makeKeyAndVisible
+
+    @add_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @add_button.setTitle("Random Location", forState:UIControlStateNormal)
+    @add_button.sizeToFit
+    @add_button.frame = CGRect.new(
+      [10, @window.frame.size.height - 10 - @add_button.frame.size.height],
+      @add_button.frame.size)
+    @add_button.addTarget(self,
+                          action: 'randomLocation',
+                          forControlEvents: UIControlEventTouchUpInside)
+    @window.addSubview(@add_button)
+
     true
   end
 end
